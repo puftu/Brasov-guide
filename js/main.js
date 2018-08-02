21428da1c9c1a8bb12343c4d2d0eb0468be66977
Apetrei Mihaela <carabus_mihaela88@yahoo.com>
2018-08-02 19:24:31 +0300

    Delete unnecessary css folder and modify index, contact, shopping

--- js/main.js
+++ js/main.js
@@ -24,6 +24,79 @@ $(function() {
       }
     });
     router.on({
+      'shopping': function() {
+        console.log("shopping");
+
+        function initMarkerData() {
+          window.markerData = [{
+              name: "Coresi Shopping Resort",
+              picture: "coresi.jpg",
+              index: 0,
+              id: "coresi",
+              rating: 4.6,
+              address: "Strada Zaharia Stancu 1, Brașov",
+              website: "https://www.coresibrasov.ro/",
+              nameWebSite: "coresibrasov.ro",
+              phone: "0736 738 787",
+              coordonates: {
+                lat: 45.672618,
+                lng: 25.615557
+              }
+            },
+            {
+              name: "Eliana Mall",
+              picture: "eliana.jpg",
+              index: 1,
+              id: "eliana",
+              rating: 4.1,
+              address: "Strada Bazaltului 2, Brașov",
+              website: "https://eliana-mall.ro/",
+              nameWebSite: "eliana-mall.ro",
+              phone: "0268 549 073",
+              coordonates: {
+                lat: 45.656823,
+                lng: 25.561943
+              }
+            },
+            {
+              name: "Unirea Shopping Center",
+              picture: "unirea.jpg",
+              index: 2,
+              id: "unirea",
+              rating: 3.6,
+              address: "Bulevardul Gării 3A, Brașov",
+              website: "http://www.unireashop.ro/",
+              nameWebSite: "unireashop.ro",
+              phone: "0268 301 030",
+              coordonates: {
+                lat: 45.661133,
+                lng: 25.608403
+              }
+            },
+            {
+              name: "Star Shopping Center",
+              picture: "star.jpg",
+              index: 3,
+              id: "star",
+              rating: 4.7,
+              address: "Strada Nicolae Bălcescu 62, Brașov",
+              website: "http://www.starcom.ro/",
+              nameWebSite: "starcom.ro",
+              phone: "0268 419 323",
+              coordonates: {
+                lat: 45.643681,
+                lng: 25.597442
+              }
+            },
+          ];
+        }
+
+        initMarkerData();
+        $("#mainContainer").load("shopping/shopping.html", null, initPage);
+
+      }
+    });
+    router.on({
       'restaurants': function() {
         console.log("restaurants");
         $("#mainContainer").load("restaurants/restaurant.html");
@@ -43,8 +116,6 @@ $(function() {
   }
 
   // Pause and play the video, and change the button text
-
-
   function initVideo() {
     var $video = $("#myVideo");
 
@@ -58,4 +129,19 @@ $(function() {
       $btn.html("Play");
     }
   }
+});
+
+
+// gallery
+$(function() {
+  var selectedClass = "";
+  $(".filter").click(function() {
+    selectedClass = $(this).attr("data-rel");
+    $("#gallery").fadeTo(100, 0.1);
+    $("#gallery div").not("." + selectedClass).fadeOut().removeClass('animation');
+    setTimeout(function() {
+      $("." + selectedClass).fadeIn().addClass('animation');
+      $("#gallery").fadeTo(300, 1);
+    }, 300);
+  });
 });
