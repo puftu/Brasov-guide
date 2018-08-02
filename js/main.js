826f8c6961b0932d72e3fe848e4575dcbe92e42f
Apetrei Mihaela <carabus_mihaela88@yahoo.com>
2018-08-02 20:13:33 +0300

    Add places resources

--- js/main.js
+++ js/main.js
@@ -27,79 +27,26 @@ $(function() {
       'shopping': function() {
         console.log("shopping");
 
-        function initMarkerData() {
-          window.markerData = [{
-              name: "Coresi Shopping Resort",
-              picture: "coresi.jpg",
-              index: 0,
-              id: "coresi",
-              rating: 4.6,
-              address: "Strada Zaharia Stancu 1, Brașov",
-              website: "https://www.coresibrasov.ro/",
-              nameWebSite: "coresibrasov.ro",
-              phone: "0736 738 787",
-              coordonates: {
-                lat: 45.672618,
-                lng: 25.615557
-              }
-            },
-            {
-              name: "Eliana Mall",
-              picture: "eliana.jpg",
-              index: 1,
-              id: "eliana",
-              rating: 4.1,
-              address: "Strada Bazaltului 2, Brașov",
-              website: "https://eliana-mall.ro/",
-              nameWebSite: "eliana-mall.ro",
-              phone: "0268 549 073",
-              coordonates: {
-                lat: 45.656823,
-                lng: 25.561943
-              }
-            },
-            {
-              name: "Unirea Shopping Center",
-              picture: "unirea.jpg",
-              index: 2,
-              id: "unirea",
-              rating: 3.6,
-              address: "Bulevardul Gării 3A, Brașov",
-              website: "http://www.unireashop.ro/",
-              nameWebSite: "unireashop.ro",
-              phone: "0268 301 030",
-              coordonates: {
-                lat: 45.661133,
-                lng: 25.608403
-              }
-            },
-            {
-              name: "Star Shopping Center",
-              picture: "star.jpg",
-              index: 3,
-              id: "star",
-              rating: 4.7,
-              address: "Strada Nicolae Bălcescu 62, Brașov",
-              website: "http://www.starcom.ro/",
-              nameWebSite: "starcom.ro",
-              phone: "0268 419 323",
-              coordonates: {
-                lat: 45.643681,
-                lng: 25.597442
-              }
-            },
-          ];
-        }
-
-        initMarkerData();
-        $("#mainContainer").load("shopping/shopping.html", null, initPage);
-
+        // window.markerData =
+        $.ajax({
+          url: "data/shopping.json",
+        }).then(function(data) {
+          console.log(data);
+          window.markerData = data;
+          $("#mainContainer").load("places/places.html", null, initPage);
+        });
       }
     });
     router.on({
       'restaurants': function() {
         console.log("restaurants");
-        $("#mainContainer").load("restaurants/restaurant.html");
+        $.ajax({
+          url: "data/restaurants.json",
+        }).then(function(data) {
+          console.log(data);
+          window.markerData = data;
+          $("#mainContainer").load("places/places.html", null, initPage);
+        });
       }
     });
     router.on({
