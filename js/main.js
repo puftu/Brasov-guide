29688bd25dd4cfe0bdee3765d6b1ce6fe3b1bcc8
Apetrei Mihaela <carabus_mihaela88@yahoo.com>
2018-08-05 19:03:09 +0300

    delete shopping, hotels, restaurants folder and modify css after web designe

--- js/main.js
+++ js/main.js
@@ -102,18 +102,3 @@ $(function() {
     }
   }
 });
-
-
-// gallery
-$(function() {
-  var selectedClass = "";
-  $(".filter").click(function() {
-    selectedClass = $(this).attr("data-rel");
-    $("#gallery").fadeTo(100, 0.1);
-    $("#gallery div").not("." + selectedClass).fadeOut().removeClass('animation');
-    setTimeout(function() {
-      $("." + selectedClass).fadeIn().addClass('animation');
-      $("#gallery").fadeTo(300, 1);
-    }, 300);
-  });
-});
